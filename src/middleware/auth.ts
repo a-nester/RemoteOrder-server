6fe34560f6c9740d0a9397d76923bd6c1aef6035
a-nester <a_nester@ukr.net>
2026-02-08 00:38:13 +0200

    fix

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const ADMIN_SECRET = process.env.ADMIN_SECRET || 'secret';
+const ADMIN_SECRET = process.env.ADMIN_SECRET;
 
 export const adminAuth = (req: Request, res: Response, next: NextFunction) => {
     const secret = req.headers['x-admin-secret'];
